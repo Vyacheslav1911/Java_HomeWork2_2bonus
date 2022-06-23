12e22e43c366ad87f80fa383b5306579237b29df
Vyacheslav1911 <slavagavrilov99@gmail.com>
2022-06-23 17:59:31 +0300

    balance_bonus

---
+++ src/Pay_bonus.java
@@ -0,0 +1,17 @@
+import java.sql.SQLOutput;
+
+public class Pay_bonus {
+    public static void main(String args[]) {
+        int balance = 100;
+        int deposit = 1600;
+        int bonus = 0;
+
+        if (deposit >= 1000) {
+            bonus = deposit / 100;
+        } else {
+            bonus = 0;
+        }
+        balance = balance + deposit + bonus;
+        System.out.println("Итоговый счет: " + balance + " руб. Бонус: " + bonus + " руб.");
+    }
+}
