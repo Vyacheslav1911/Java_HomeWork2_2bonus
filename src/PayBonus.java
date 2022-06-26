acf50b2a48423e40a5acb68a184cc9ad952d7514
Vyacheslav1911 <slavagavrilov99@gmail.com>
2022-06-26 11:50:57 +0300

    rename and adit

---
+++ src/PayBonus.java
@@ -0,0 +1,16 @@
+public class PayBonus {
+    public static void main(String[] args) {
+        int balance = 100;
+        int deposit = 1_600;
+        int bonus = 0;
+
+        if (deposit > 1_000) {
+            bonus = deposit / 100;
+        } else {
+            bonus = 0;
+        }
+        int result = balance + deposit + bonus;
+
+        System.out.println("Итоговый счет: " + result + " руб. Бонус: " + bonus + " руб.");
+    }
+}
